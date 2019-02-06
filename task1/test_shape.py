baf60bc19e1cebbebb2e114dbe0809a8003d68b8
Yawen Han <yawenhan@Yawens-Air.home>
2019-02-06 16:53:20 -0500

    update

--- task1/test_shape.py
+++ task1/test_shape.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def test_rownum():
-    df=pd.read_csv("input.txt",encoding='utf-16be',escapechar='\\',na_values='--',index_col=0)
+    df=pd.read_csv("./task1/input.txt",encoding='utf-16be',escapechar='\\',na_values='--',index_col=0)
     assert len(df.columns)==31
     assert len(df.index)==225
-
-
 
--- task1/test_totalPopulation.py
+++ task1/test_totalPopulation.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def test_totalPopulation():
-    df=pd.read_csv("input.txt",encoding='utf-16be',escapechar='\\',na_values='--',index_col=0)
+    df=pd.read_csv("./task1/input.txt",encoding='utf-16be',escapechar='\\',na_values='--',index_col=0)
     assert int(df["2010"].sum())==7065
